81102301979991e0ea664c8564998965316f8293
Shrihari <shriharit2004@gmail.com>
2024-03-19 19:08:26 +0530

    First commit of TicTacToe game using HTML,CSS,JS

---
+++ app.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let turn1 = true; //playerX, playerO
+
+let newGameBtn = document.querySelector("#new-btn");
+let msgCont = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+
+msg.hidden = true;
+
+const showWiner = (winner) => {
+    msg.innerText = `Congrats, Winner is ${winner} !`;
+    // msgCont.classList.remove("hide");
+    msg.hidden = false;
+
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+
+}
+
+const winPaterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box clicked");
+        if (turn1) {
+            box.innerText = "X";
+            turn1 = false;
+        } else {
+            box.innerText = "O";
+            turn1 = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    })
+});
+
+const checkWinner = function () {
+    for (pattern of winPaterns) {
+        let p1Val = boxes[pattern[0]].innerText;
+        let p2Val = boxes[pattern[1]].innerText;
+        let p3Val = boxes[pattern[2]].innerText;
+
+        if (p1Val != "" && p2Val != "" && p3Val != "") {
+            if (p1Val === p2Val && p2Val === p3Val)
+                showWiner(p1Val);
+        }
+    }
+
+
+}
+
+
+const resetGame  = ()=>{
+   turn1 = true;
+   for(let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+    }
+    msg.hidden = true;
+}
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
